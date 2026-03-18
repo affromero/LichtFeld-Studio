133d960b9de7be5d624e068b33515b6b2b104028
Miriam <yimeng630@gmail.com>
2026-03-17 23:38:35 -0700

    windows: fix test linking (#1006)

--- src/visualizer/input/sdl_key_mapping.hpp
+++ src/visualizer/input/sdl_key_mapping.hpp
@@ -4,15 +4,16 @@
 
 #pragma once
 
+#include "core/export.hpp"
 #include <SDL3/SDL_keycode.h>
 #include <SDL3/SDL_mouse.h>
 #include <SDL3/SDL_scancode.h>
 
 namespace lfs::vis::input {
 
-    int sdlScancodeToAppKey(SDL_Scancode scancode);
-    int sdlModsToAppMods(SDL_Keymod sdl_mods);
-    int sdlMouseButtonToApp(uint8_t sdl_button);
-    SDL_Scancode appKeyToSdlScancode(int app_key);
+    LFS_VIS_API int sdlScancodeToAppKey(SDL_Scancode scancode);
+    LFS_VIS_API int sdlModsToAppMods(SDL_Keymod sdl_mods);
+    LFS_VIS_API int sdlMouseButtonToApp(uint8_t sdl_button);
+    LFS_VIS_API SDL_Scancode appKeyToSdlScancode(int app_key);
 
 } // namespace lfs::vis::input
